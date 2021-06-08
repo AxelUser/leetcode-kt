191cc40b336d18262604c9deb8864a9a135b625b
Alexey Maltsev <axel.user.perm@gmail.com>
2021-06-09 01:11:49 +0300

    Solved findMedianSortedArrays

---
+++ src/main/kotlin/findMedianSortedArrays/Solution.kt
@@ -0,0 +1,39 @@
+package findMedianSortedArrays
+
+class Solution {
+    fun findMedianSortedArrays(nums1: IntArray, nums2: IntArray): Double {
+        val merged = mutableListOf<Int>()
+        var idx1 = 0
+        var idx2 = 0
+
+        while (idx1 < nums1.size || idx2 < nums2.size) {
+            val first = if (idx1 < nums1.size) nums1[idx1] else null
+            val second = if (idx2 < nums2.size) nums2[idx2] else null
+
+            when{
+                first == null -> {
+                    merged.add(second!!)
+                    idx2++
+                }
+                second == null -> {
+                    merged.add(first)
+                    idx1++
+                }
+                first <= second -> {
+                    merged.add(first)
+                    idx1++
+                }
+                second < first -> {
+                    merged.add(second)
+                    idx2++
+                }
+            }
+        }
+
+        return if (merged.size % 2 == 0) {
+            (merged[merged.size / 2 - 1] + merged[merged.size / 2]) / 2.0
+        } else {
+            merged[merged.size / 2].toDouble()
+        }
+    }
+}
---
+++ src/test/kotlin/findMedianSortedArrays/FindMedianSortedArraysTest.kt
@@ -0,0 +1,31 @@
+package findMedianSortedArrays
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+
+internal class FindMedianSortedArraysTest {
+
+    private val sut = Solution()
+
+    @ParameterizedTest
+    @MethodSource
+    fun findMedianSortedArrays(nums1: IntArray, nums2: IntArray, expected: Double) {
+        val actual = sut.findMedianSortedArrays(nums1, nums2)
+
+        assertEquals(expected, actual)
+    }
+
+    companion object {
+        @JvmStatic
+        fun findMedianSortedArrays() = listOf(
+            Arguments.of(arrayOf(1, 3, 5, 6).toIntArray(), arrayOf(2, 4, 7, 8).toIntArray(), 4.5),
+            Arguments.of(arrayOf(1, 3).toIntArray(), arrayOf(2).toIntArray(), 2.0),
+            Arguments.of(arrayOf(1, 2).toIntArray(), arrayOf(3, 4).toIntArray(), 2.5),
+            Arguments.of(arrayOf(0, 0).toIntArray(), arrayOf(0, 0).toIntArray(), 0.0),
+            Arguments.of(IntArray(0), arrayOf(1).toIntArray(), 1.0),
+            Arguments.of(arrayOf(2).toIntArray(), IntArray(0), 2.0)
+        )
+    }
+}
