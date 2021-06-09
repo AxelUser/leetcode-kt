9bc9b3c43a3f857a7291bb102b8cab0dec6f23b1
Alexey Maltsev <axel.user.perm@gmail.com>
2021-06-10 01:45:24 +0300

    Solved reverse integer

---
+++ src/main/kotlin/reverseInt/Solution.kt
@@ -0,0 +1,21 @@
+package reverseInt
+
+class Solution {
+    fun reverse(x: Int): Int {
+        var res = 0
+        var n = x
+        while (n != 0) {
+            val dig = n % 10
+            n /= 10
+
+            when {
+                res > Int.MAX_VALUE / 10 || (res == Int.MAX_VALUE / 10 && dig > 7) -> return 0
+                res < Int.MIN_VALUE / 10 || (res == Int.MIN_VALUE / 10 && dig < -8) -> return 0
+            }
+
+            res = res * 10 + dig
+        }
+
+        return res
+    }
+}
