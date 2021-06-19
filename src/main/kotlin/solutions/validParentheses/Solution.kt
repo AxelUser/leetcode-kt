e2a203c09e400d967b6784f11a7a1b76ba9151d6
Alexey Maltsev <axel.user.perm@gmail.com>
2021-06-19 16:17:38 +0300

    Solved Valid Parentheses

---
+++ src/main/kotlin/solutions/validParentheses/Solution.kt
@@ -0,0 +1,29 @@
+package solutions.validParentheses
+
+import readmeGeneration.ProblemDifficulty
+import readmeGeneration.ProblemSolution
+import java.util.*
+
+@ProblemSolution(20, "Valid Parentheses", ProblemDifficulty.EASY, "https://leetcode.com/problems/valid-parentheses/")
+class Solution {
+    fun isValid(s: String): Boolean {
+        val parentheses = Stack<Char>()
+
+        for (c in s) {
+            when (c) {
+                '(', '[', '{' -> parentheses.push(c)
+                ')' -> {
+                    if (parentheses.empty() || parentheses.pop() != '(') return false
+                }
+                ']' -> {
+                    if (parentheses.empty() || parentheses.pop() != '[') return false
+                }
+                '}' -> {
+                    if (parentheses.empty() || parentheses.pop() != '{') return false
+                }
+            }
+        }
+
+        return parentheses.empty()
+    }
+}
