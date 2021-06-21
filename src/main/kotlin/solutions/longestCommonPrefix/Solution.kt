412957d3f08d0f343f86ab753a8d6346dc30cf79
Alexey Maltsev <axel.user.perm@gmail.com>
2021-06-22 00:50:44 +0300

    Solved Longest Common Prefix

---
+++ src/main/kotlin/solutions/longestCommonPrefix/Solution.kt
@@ -0,0 +1,24 @@
+package solutions.longestCommonPrefix
+
+import readmeGeneration.ProblemDifficulty
+import readmeGeneration.ProblemSolution
+
+@ProblemSolution(14, "Longest Common Prefix", ProblemDifficulty.EASY,
+    "https://leetcode.com/problems/longest-common-prefix/")
+class Solution {
+    fun longestCommonPrefix(strs: Array<String>): String {
+        if(strs.isEmpty()) return ""
+
+        val n = strs.minOf { it.length }
+        var common = 0
+
+        for (i in 0 until n) {
+            val c = strs[0][i]
+            if(strs.any { it[i] != c }) break
+
+            common++
+        }
+
+        return if (common == 0) "" else strs[0].substring(0, common)
+    }
+}
---
+++ src/test/kotlin/solutions/longestCommonPrefix/LongestCommonPrefixTest.kt
@@ -0,0 +1,24 @@
+package solutions.longestCommonPrefix
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+
+internal class LongestCommonPrefixTest {
+
+    @ParameterizedTest
+    @MethodSource
+    fun longestCommonPrefix(strs: Array<String>, expected: String) {
+        assertEquals(expected, Solution().longestCommonPrefix(strs))
+    }
+
+    companion object {
+        @JvmStatic
+        fun longestCommonPrefix() = listOf(
+            Arguments.of(arrayOf("flower","flow","flight"), "fl"),
+            Arguments.of(arrayOf("dog","racecar","car"), ""),
+            Arguments.of(arrayOf("cir","car"), "c"),
+        )
+    }
+}
