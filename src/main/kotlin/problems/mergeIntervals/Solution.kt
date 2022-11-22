dc9d97df66f947fd50faaba9101190697d348d3a
Alexey Maltsev <axel.user.perm@gmail.com>
2022-11-23 00:04:26 +0300

    Refactored sorting in Merge Intervals

--- src/main/kotlin/problems/mergeIntervals/Solution.kt
+++ src/main/kotlin/problems/mergeIntervals/Solution.kt
@@ -2,12 +2,11 @@ package problems.mergeIntervals
 
 import readmeGeneration.ProblemDifficulty
 import readmeGeneration.ProblemSolution
-import java.util.*
 
 @ProblemSolution(56, "Merge Intervals", ProblemDifficulty.MEDIUM, "https://leetcode.com/problems/merge-intervals/")
 class Solution {
     fun merge(intervals: Array<IntArray>): Array<IntArray> {
-        Arrays.sort(intervals) { arr1, arr2 -> arr1[0].compareTo(arr2[0]) }
+        intervals.sortBy { it[0] }
         val merged = mutableListOf<IntArray>()
 
         for (interval in intervals) {
